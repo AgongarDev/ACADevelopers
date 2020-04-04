7b1f1612570f4916f42778129149e9ecb3ba2012
Antonio González <58194284+AgongarDev@users.noreply.github.com>
2020-04-04 05:08:52 +0200

    Creada la clase AdministracionFisica.
    He creado la clase AdministracionFisica basándome en el diagrama de
    clases del producto1.
    
    He dejado numEmpleados como un entero pero si pudiéramos en un futuro
    corregirlo, creo que sería mejor añadir una lista de empleados y obtener
    el número de contar los valores de la lista.
    
    Obtendríamos más información.
    
    No considero necesario crear las clases que la especializan porque no
    tienen atributos ni métodos declarados.

--- EntreCulturas/src/sistema/basics/AdministracionFisica.java
+++ EntreCulturas/src/sistema/basics/AdministracionFisica.java
@@ -2,4 +2,69 @@ package sistema.basics;
 
 public class AdministracionFisica {
 
+	private String idAdmin;
+	private String direccion;
+	private int numEmpleados;
+	private String correo;
+	private int telefono;
+	
+	//constructores
+	
+	public AdministracionFisica () {
+		super();
+	}
+	
+	public AdministracionFisica (String id, String direccion, int empleados, String correo, int telefono) {
+		this.idAdmin = id;
+		this.direccion = direccion;
+		this.numEmpleados = empleados;
+		this.correo = correo;
+		this.telefono = telefono;
+	}
+	
+	//getters y setters
+
+	public String getIdAdmin() {
+		return idAdmin;
+	}
+
+	public void setIdAdmin(String idAdmin) {
+		this.idAdmin = idAdmin;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getNumEmpleados() {
+		return numEmpleados;
+	}
+
+	public void setNumEmpleados(int numEmpleados) {
+		this.numEmpleados = numEmpleados;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	
+
+	
+	
 }
