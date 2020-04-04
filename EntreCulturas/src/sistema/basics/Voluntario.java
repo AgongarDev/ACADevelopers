f1a611e98a8f7284a5c664f2705bd64e74bde2cc
Antonio González <58194284+AgongarDev@users.noreply.github.com>
2020-04-04 06:18:27 +0200

    Creadas las clases Voluntario y Colaborador.
    He creado las clases Trabajador y Colaborador siguiendo el diagrama de
    clases del producto1.
    
    No considero necesario crear las especializaciones de Voluntario
    (Nacional, Internacional) al menos por el momento.
    Asimismo, puesto que Ninguna de las dos clases tiene más atributos que
    los de persona, creo que sería interesante convertir estas clases en
    simples atributos, quizás de algún tipo enum.
    
    Otra opción sería convertir la clase Persona en una clase Abstracta.

---
+++ EntreCulturas/src/sistema/basics/Voluntario.java
@@ -0,0 +1,15 @@
+package sistema.basics;
+
+public class Voluntario extends Persona {
+	
+	public Voluntario () {
+		super();
+	}
+
+	public Voluntario(String dni, String nombre, String apellidos, String telefono, String domicilio,
+			String fechaInicio, String fechaFin, AdministracionFisica sedeAsignada, String cargo, String correo,
+			Ong ong) {
+		super(dni, nombre, apellidos, telefono, domicilio, fechaInicio, fechaFin, sedeAsignada, cargo, correo, ong);
+}
+
+}
