4e50fa3b585864231e350414f985a7e7f3e824d6
AnaIgsan <61803230+AnaIgsan@users.noreply.github.com>
2020-05-08 18:45:37 +0200

    Añadida la utilidad hibernate
    Crea un objeto SessionFactory a partir del cual se creará la sessión y sus métodos a partir de los cuales persistiremos

--- EntreCulturas/src/main/java/acadevs/entreculturas/modelo/SessionFactory.java
+++
@@ -1,8 +0,0 @@
-package acadevs.entreculturas.modelo;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-
